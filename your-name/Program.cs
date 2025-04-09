89e38e9e61f080816d8990557635cde58ba94a59
danichagas <chagasdaniel788@gmail.com>
2025-04-08 23:00:08 -0300

    feat: finished program

--- your-name/Program.cs
+++ your-name/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace yourName;
+
+class Program
+{
+   public static void Main()
+    {
+        Console.WriteLine("Digite seu nome: ");
+        var name = Console.ReadLine();
+
+        Console.WriteLine($"Olá {name}!, seja muito bem vindo!");
+
+    }
+}
